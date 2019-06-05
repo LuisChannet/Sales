57faa36b7daf1c0964a2b1a634379be2a8d85c61
Channet <channet@inteligenciatotal.com>
2019-06-05 00:58:26 -0500

    segundo

---
+++ Sales/Sales/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿namespace Sales.Infrastructure
+{
+using Sales.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Sales.ViewModels
+{
+    using Sales.Common.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Text;
+
+    public class ProductsViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+    }
+}
